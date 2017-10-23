122b02a3d96c755881595d754270b783701c64fb
Lochanahasith <lochanahm@gmail.com>
2017-10-23 13:41:41 +0530

    login controller added

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	public function __construct(){
+	public function __construct() {
 		parent::__construct();
 		// $this->session->keep_flashdata('success');
 		// $this->session->keep_flashdata('errordb');
@@ -11,54 +11,81 @@ class Welcome extends CI_Controller {
 
 	}
 
-	public function index()
-	{
+	public function index() {
 
 		$this->load->view('home');
 	}
 
-	public function registerUser(){
+	public function registerUser() {
 
 		$this->load->library('form_validation');
 
-		$this->form_validation->set_rules('name','Name','required');
-		$this->form_validation->set_rules('username','User name','required|is_unique[user.username]');
-		$this->form_validation->set_rules('email','Email','trim|required|valid_email|is_unique[user.email]');
+		$this->form_validation->set_rules('name', 'Name', 'required');
+		$this->form_validation->set_rules('username', 'User name', 'required|is_unique[user.username]');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[user.email]');
 		//$this->form_validation->set_rules('password','Password','required');
 		//$this->form_validation->set_rules('confirm_password','Confirm Password','required|matches[password]');
 
+		if ($this->form_validation->run() == TRUE) {
 
+			$name = ucwords(strtolower($this->input->post('name')));
+			$username = ucwords(strtolower($this->input->post('username')));
+			$email = $this->input->post('email');
+			$password = $this->input->post('password');
+			$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
+			$userInfo = array('name' => $name, 'username' => $username, 'email' => $email, 'password' => $hashedPassword);
 
-		if($this->form_validation->run()==TRUE){
+			$this->load->model('User');
+			$result = $this->User->insertUser($userInfo);
+			$this->session->set_flashdata('success', 'You have registered successfully');
+			if ($result > 0) {
+				$this->session->set_flashdata('success', 'You have registered successfully');
+			} else {
+				$this->session->set_flashdata('errordb', 'Error in database insertion');
+			}
 
-				$name = ucwords(strtolower($this->input->post('name')));
-		    $username = ucwords(strtolower($this->input->post('username')));
-            $email = $this->input->post('email');
-            $password = $this->input->post('password');
-            $hashedPassword=password_hash($password, PASSWORD_DEFAULT);
-
-            $userInfo = array('name'=>$name,'username' => $username,'email'=>$email,'password'=>$hashedPassword,);
-
-            $this->load->model('User');
-            $result = $this->User->insertUser($userInfo);
-						$this->session->set_flashdata('success', 'You have registered successfully');
-            if($result>0){
-            	$this->session->set_flashdata('success', 'You have registered successfully');
-            }else{
-            	$this->session->set_flashdata('errordb', 'Error in database insertion');
-            }
-
-            redirect('','refresh');
-		}else{
+			redirect('', 'refresh');
+		} else {
 			$this->session->set_flashdata('error', 'Error in fuck registration');
 
-
-			redirect('','refresh');
+			redirect('', 'refresh');
 
 		}
 
-
-
 	}
+
+	public function loginUser() {
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('username', 'Username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('home');
+		} else {
+			$this->load->model('User');
+			$result = $this->User->loginUser();
+
+			if ($result != false) {
+				$user_data = array(
+					'user_id' => $result->user_id,
+					'name' => $result->name,
+					'username' => $result->username,
+					'email' => $result->email,
+					'loggedin' => TRUE,
+
+				);
+				$this->session->set_userdata($user_data);
+				print_r($_SESSION);
+				//redirect('Welcome/adController');
+
+			} else {
+				$this->session->set_flashdata('errmsg', 'Invalid Username or Password');
+				redirect('Welcome/loginUser');
+
+			}
+
+		}
+	}
+
 }
