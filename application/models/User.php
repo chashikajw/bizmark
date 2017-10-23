143d98b0c749b0bf5ed82454a3894bb477adce92
Lochanahasith <lochanahm@gmail.com>
2017-10-23 13:43:31 +0530

    login function in model added

--- application/models/User.php
+++ application/models/User.php
@@ -1,21 +1,39 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User extends CI_Model{
+class User extends CI_Model {
 
-	public function insertUser($userInfo){
+	public function insertUser($userInfo) {
 
 		try {
-            $this->db->insert('user', $userInfo);
+			$this->db->insert('user', $userInfo);
 
-            $ret = $this->db->insert_id() + 0;
-            return $ret;
-        } catch (Exception $err) {
-            return $err->getMessage();
-        }
+			$ret = $this->db->insert_id() + 0;
+			return $ret;
+		} catch (Exception $err) {
+			return $err->getMessage();
+		}
 
 	}
 
+	public function loginUser() {
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		//$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
+		$this->db->where('username', $username);
+		$this->db->where('password', $password);
+		//$this->db->where('password', $hashedPassword);
+
+		$respond = $this->db->get('user');
+
+		if ($respond->num_rows() == 1) {
+			return $respond->row(0);
+
+		} else {
+			return false;
+		}
+
+	}
 
 }
