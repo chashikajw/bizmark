7787e5407b37fcf172f203d739449348034e9704
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-10-26 03:48:56 +0530

    Add base template

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -1,8 +1,18 @@
 <?php
-class Pages extends CI_Controller {
+class Client extends CI_Controller {
 
         public function view($page = 'home')
         {
+            // if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+            // {
+            //     // Whoops, we don't have a page for that!
+            //     show_404();
+            // }
 
+            $data['title'] = ucfirst($page); // Capitalize the first letter
+
+            $this->load->view('client/layouts/client_header', $data);
+            $this->load->view('client/pages/'.$page, $data);
+            $this->load->view('client/layouts/client_footer', $data);
         }
 }
