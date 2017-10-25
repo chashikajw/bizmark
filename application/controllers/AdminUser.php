0e2a968ed1aec076bce152a14365c9ce3460461a
chashikajw <chashikajw007@gmail.com>
2017-10-26 04:29:32 +0530

    add adminuser controller functions

--- application/controllers/AdminUser.php
+++ application/controllers/AdminUser.php
@@ -13,6 +13,60 @@ class Welcome extends CI_Controller {
 
 	public function index() {
 
-		$this->load->view('home');
+		$this->load->view('mapview');
 	}
+
+	public function postview(){
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/sidebar');
+		$this->load->view('Adminuser/post');
+		$this->load->view('layouts/footer');  				
+	}
+
+	
+	public function dashboardView() {
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/sidebar');
+		$this->load->view('Adminuser/dashboard');
+		$this->load->view('layouts/footer');
+	}
+
+
+	public function configurationview(){
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/sidebar');
+		$this->load->view('Adminuser/configuration');
+		$this->load->view('layouts/footer');  				
+	}
+
+
+	public function profileView() {
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/sidebar');
+		$this->load->view('Adminuser/sProfile');
+		$this->load->view('layouts/footer');
+	}
+
+	public function inboxView() {
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/sidebar');
+		$this->load->view('Adminuser/inbox');
+		$this->load->view('layouts/footer');
+	}
+
+	public function shopRegView() {
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/sidebar');
+		$this->load->view('Adminuser/shopRegistration');
+		$this->load->view('layouts/footer');
+	}
+
+	public function review() {
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/sidebar');
+		$this->load->view('Adminuser/reviews');
+		$this->load->view('layouts/footer');
+	}
+
+
 }
