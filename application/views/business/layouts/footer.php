2c28f6f097c8928f2e6c26cc431fb33426c765fc
Lochanahasith <lochanahm@gmail.com>
2017-10-26 18:13:36 +0530

    testing

--- application/views/business/layouts/footer.php
+++ application/views/business/layouts/footer.php
@@ -40,7 +40,7 @@
     <!-- bootstrap-daterangepicker -->
     <script src="<?php echo base_url(); ?>assets/vendors/moment/min/moment.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
-    
+
     <!-- Custom Theme Scripts -->
     <script src="<?php echo base_url(); ?>assets/build/js/custom.min.js"></script>
   </body>
