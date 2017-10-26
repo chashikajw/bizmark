1713f3950215a2c7e29ad6e96874bea0b67cf1be
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-10-26 11:26:50 +0530

    Add signup page

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -1,13 +1,26 @@
 <?php
 class Client extends CI_Controller {
 
-        public function index()
-        {
-            $page = "home";
-            $data['title'] = ucfirst($page); // Capitalize the first letter
+    public function index(){
+        $this->showPage('home');
+    }
 
-            $this->load->view('client/layouts/client_header', $data);
-            $this->load->view('client/pages/'.$page, $data);
-            $this->load->view('client/layouts/client_footer', $data);
+    public function signup(){
+        $this->showPage('signup');
+    }
+
+
+    public function showPage($page)
+    {
+        if ( ! file_exists(APPPATH.'views/client/pages/'.$page.'.php'))
+        {
+            // Whoops, we don't have a page for that!
+            show_404();
         }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+        $this->load->view('client/layouts/client_header', $data);
+        $this->load->view('client/pages/'.$page, $data);
+        $this->load->view('client/layouts/client_footer', $data);
+    }
 }
