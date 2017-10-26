be2f6cbd95ce9c391597df81bc01c5154246bd43
Lochanahasith <lochanahm@gmail.com>
2017-10-27 03:36:50 +0530

    feilds updated

--- application/views/business/business_registration.php
+++ application/views/business/business_registration.php
@@ -28,7 +28,7 @@
                   </div>
                   <div class="x_content">
                     <br />
-                    <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+                    <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" action="<?php echo base_url() ?>index.php/Business/shopRegistration" method="post" >
 
                       <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name"> Name
@@ -66,12 +66,7 @@
                         </div>
                       </div>
 
-                      <div class="form-group">
-                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="logopath">Logo Path</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input type="text" id="logopath" name="logopath" required="required" class="form-control col-md-7 col-xs-12">
-                        </div>
-                      </div>
+
 
                       <div class="form-group">
                         <label for="openingTime" class="control-label col-md-3 col-sm-3 col-xs-12">Opening Time</label>
