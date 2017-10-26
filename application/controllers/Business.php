670ded8ed3e42fae061bec84072ab23bcbdd71de
Lochanahasith <lochanahm@gmail.com>
2017-10-27 03:35:58 +0530

    business controller updated

--- application/controllers/Business.php
+++ application/controllers/Business.php
@@ -15,20 +15,17 @@ class Business extends CI_Controller {
 		$this->showPage('business_profile');
 	}
 
-	public function news_feed(){
+	public function news_feed() {
 		$this->showPage('news_feed');
 	}
-
 
 	public function dashboard() {
 		$this->showPage('dashboard');
 	}
 
-
-	public function configuration(){
+	public function configuration() {
 		$this->showPage('configuration');
 	}
-
 
 	public function profile() {
 		$this->showPage('business_profile');
@@ -45,21 +42,62 @@ class Business extends CI_Controller {
 	public function review() {
 		$this->showPage('reviews');
 	}
+	public function shopRegistration() {
+		$this->load->library('form_validation');
 
+		$this->form_validation->set_rules('name', 'User name', 'required');
+		$this->form_validation->set_rules('handler', 'Name', 'required');
+		$this->form_validation->set_rules('address', 'Address', 'required');
+		$this->form_validation->set_rules('city', 'City', 'required');
+		$this->form_validation->set_rules('cId', 'Category ID', 'required');
+		$this->form_validation->set_rules('openingTime', 'Ope', 'required');
+		$this->form_validation->set_rules('closingTime', 'City', 'required');
+		$this->form_validation->set_rules('ltd', 'City', 'required');
+		$this->form_validation->set_rules('lotd', 'City', 'required');
 
-	public function showPage($page)
-	{
-		if ( ! file_exists(APPPATH.'views/business/'.$page.'.php'))
-		{
+		if ($this->form_validation->run() == TRUE) {
+
+			$name = ucwords(strtolower($this->input->post('name')));
+			$handler = ucwords(strtolower($this->input->post('handler')));
+			$address = ucwords(strtolower($this->input->post('address')));
+			$city = $this->input->post('city');
+			$cId = $this->input->post('cId');
+			$oTime = date("Y-m-d h:i:s", $this->input->post('opening_time'));
+			$cTime = date("Y-m-d h:i:s", $this->input->post('closing_time'));
+			$ltd = $this->input->post('ltd');
+			$lotd = $this->input->post('lotd');
+
+			$shopInfo = array('name' => $name, 'handler' => $handler, 'address' => $address, '	city' => $city, 'category_id' => $cId, 'opening_time' => $oTime, 'closing_time' => $cTime, 'lat' => $ltd, 'lng' => $lotd);
+
+			$this->load->model('ClientModel');
+			$result = $this->ClientModel->insertUser($userInfo);
+
+			if ($result > 0) {
+				$this->session->set_flashdata('success', 'Your shop  has been  successfully registered');
+				redirect('business/profile');
+			} else {
+				$this->session->set_flashdata('errordb', 'Error in database insertion');
+				redirect('business/registration');
+			}
+
+		} else {
+			$this->session->set_flashdata('error', 'Error in Registration');
+
+			redirect('business/registration');
+
+		}
+	}
+
+	public function showPage($page) {
+		if (!file_exists(APPPATH . 'views/business/' . $page . '.php')) {
 			show_404();
 		}
 
 		$data['title'] = ucfirst($page); // Capitalize the first letter
 		$this->load->view('business/layouts/header');
 		$this->load->view('business/layouts/sidebar');
-		$this->load->view('business/'.$page, $data);
+		$this->load->view('business/' . $page, $data);
 		$this->load->view('business/layouts/footer');
 	}
-
 
 }
