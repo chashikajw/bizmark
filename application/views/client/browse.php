5fb8c3ca5f8e5b3c6d4ed85e16ab4d8b5cadeb55
Lochanahasith <lochanahm@gmail.com>
2017-12-15 15:09:24 +0530

    Bussiness list view added

--- application/views/client/browse.php
+++ application/views/client/browse.php
@@ -1,46 +1,48 @@
 <div class="item span9">
-  <div class="item span9">
-    <ul class="thumbnails">
+
+
+
+
 
 <?php
-$i = 0;
-foreach ($business_data as $item){ ?>
-        <a  href="">
-         <li class="span2">
-           <div class="thumbnail">
-               <?php echo "<img src='" . base_url() . "assets/business/" . $item->logo_path . "' alt=''/>" ?>
-               <div class="caption">
+
+foreach ($business_data as $item) {
+	?>
+<div class="panel panel-default" style="margin-top: 20px">
+
+<div class="container-fluide">
+
+<div class="row">
+  <div class="span8">
+
+    <div class="row">
+      <div class="span5">
+        <a href="#" class="thumbnail" style="width: 200px">
+           <?php echo "<img src='" . base_url() . "assets/business/" . $item->logo_path . "' alt=''/>" ?>
+        </a>
+      </div>
+        <div class="span2">
                  <h5><?php echo $item->name; ?></h5>
                  <p><?php echo $item->description; ?></p>
-                 <center>
-                  <button class="btn btn-flat">View</button><br>
+
+
                  <span class="fa fa-star checked"></span>
                  <span class="fa fa-star checked"></span>
                  <span class="fa fa-star checked"></span>
                  <span class="fa fa-star"></span>
                  <span class="fa fa-star"></span>
                  <!-- <h4><a class="btn" href="">VIEW</a> -->
-                 </center>
+
                      <!-- <span class="pull-right">$222.00</span></h4> -->
-               </div>
-           </div>
-         </li>
-        </a>
-<?php
-$i += 1;
-if ($i % 4 == 0){ ?>
-
-</ul>
-</div>
-<div class="item span9">
-    <ul class="thumbnails">
-
-
-<?php }
-} ?>
-
-</ul>
-</div>
-
+        </div>
+         <button class="btn btn-info" style="margin-top:40px; margin-left: 10px">View</button><br>
     </div>
+
+  </div>
 </div>
+</div>
+</div>
+<hr>
+
+<?php }?>
+
