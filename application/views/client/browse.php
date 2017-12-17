f6b6c14003a625c21440d8ec3703fae8c407e6ab
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-12-17 14:02:58 +0530

    Update database & Modify category list

--- application/views/client/browse.php
+++ application/views/client/browse.php
@@ -1,48 +1,42 @@
-<div class="item span9">
+
+<div class="item span12">
+	<div class="container main-content" >
+
+    	<?php foreach ($business_data as $item): ?>
+			<a href="#">
+			<div class="row span6 business-item" style="height: 140px">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="panel-more1 span2">
+							<a href="#" class="thumbnail panel-more1" style="width: 120px">
+								<?php echo "<img src='" . base_url() . "assets/business/" . $item->logo_path . "' alt=''/>" ?>
+							</a>
+						</div>
+						<div class="panel-info span2">
+							<p><strong><?= $item->name; ?></strong></p>
+							<p><?= $item->description; ?></p>
+						</div>
+						<div class="span2">
+							<span class="fa fa-star checked"></span>
+							<span class="fa fa-star checked"></span>
+							<span class="fa fa-star checked"></span>
+							<span class="fa fa-star"></span>
+							<span class="fa fa-star"></span>
+						</div>
+						<div class="panel-more1 span1">
+							<p><?= $item->address; ?></p>
+							<p><?= $item->city; ?></p>
+						</div>
+						<!-- <div class="panel-more1 span1">
+							<button class="btn btn-info" style="margin-top:40px; margin-left: 10px">View</button>
+						</div> -->
+					</div>
+				</div>
 
 
+			</div>
+		</a>
+		<?php endforeach; ?>
 
-
-
-<?php
-
-foreach ($business_data as $item) {
-	?>
-<div class="panel panel-default" style="margin-top: 20px">
-
-<div class="container-fluide">
-
-<div class="row">
-  <div class="span8">
-
-    <div class="row">
-      <div class="span5">
-        <a href="#" class="thumbnail" style="width: 200px">
-           <?php echo "<img src='" . base_url() . "assets/business/" . $item->logo_path . "' alt=''/>" ?>
-        </a>
-      </div>
-        <div class="span2">
-                 <h5><?php echo $item->name; ?></h5>
-                 <p><?php echo $item->description; ?></p>
-
-
-                 <span class="fa fa-star checked"></span>
-                 <span class="fa fa-star checked"></span>
-                 <span class="fa fa-star checked"></span>
-                 <span class="fa fa-star"></span>
-                 <span class="fa fa-star"></span>
-                 <!-- <h4><a class="btn" href="">VIEW</a> -->
-
-                     <!-- <span class="pull-right">$222.00</span></h4> -->
-        </div>
-         <button class="btn btn-info" style="margin-top:40px; margin-left: 10px">View</button><br>
-    </div>
-
-  </div>
+	</div>
 </div>
-</div>
-</div>
-<hr>
-
-<?php }?>
-
--- application/views/client/layouts/sidebar.php
+++ application/views/client/layouts/sidebar.php
@@ -1,5 +1,8 @@
-<ul id="myUL" class="span3">
-    <?php foreach ($categorylist as $item): ?>
-        <?= "<li ><a href=" . base_url('/Client/categary_view/') . $item->id . ">" . $item->name ."</a></li>" ?>
-    <?php endforeach; ?>
-</ul>
+
+<div style="width: 100%" class="main-content">
+    <ul id="horizontal-style">
+        <?php foreach ($categorylist as $item): ?>
+            <?= "<li ><a href=" . base_url('/Client/categary_view/') . $item->id . ">" . $item->name ."</a></li>" ?>
+        <?php endforeach; ?>
+    </ul>
+</div>
