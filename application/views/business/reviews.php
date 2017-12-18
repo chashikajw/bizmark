c77415061090962e51716262dc37516b2721d47e
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-12-18 13:40:25 +0530

    Modify rating view

--- application/views/business/reviews.php
+++ application/views/business/reviews.php
@@ -4,56 +4,45 @@
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>User ratings</h3>
+                <h3>Ratings & Reviews</h3>
               </div>
 
-            
+
+
+
             <div class="clearfix"></div>
 
             <div class="row">
               <div class="col-md-12">
                 <div class="x_panel">
                   <div class="x_content">
-                     <center>
+                     <!-- <center> -->
                     <div class="row">
-                     
-
-                      <div class="clearfix"></div>
-                     
-                      <div class="col-md-4 col-sm-8 col-xs-12 profile_details">
-                         <div class="col-md-12">
-                <div class="x_panel">
-                    <div class="x_title">
-                        <h2>Reviews</h2>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="x_content">
 
                         <!-- Add items -->
                        <?php foreach ($review as $item): ?>
                             <article class="media event">
                                 <a class="pull-left date">
-                                    <p class="day">
-                                        <small> <span class="fa fa-star checked"></span></small><?= $item->value; ?>
-                                    </p>
+                                    <?php $datetime = new DateTime($item->datetime); ?>
+                                    <p class="month"><?= $datetime->format('M'); ?></p>
+                                    <p class="day"><?= $datetime->format('d'); ?></p>
                                 </a>
                                 <div class="media-body">
                                     <a class="title" href="#"> <?= $item->name; ?> </a>
                                     <p> <?= $item->comment; ?> </p>
-                                    <p> <?= $item->datetime; ?> </p>
-                                </div>
+
+                                <?php
+                                    for ($i=0; $i<5; $i++){
+                                        echo "<span class='fa " . ($item->value > $i ? 'fa-star' : 'fa-star-o') . "'></span>";
+                                    }
+                                 ?>
+                             </div>
                             </article>
                         <?php endforeach; ?>
-
-
-                    </div>
-                </div>
-            </div>
-                    
                     </div>
 
                   </div>
-                  </center>
+                  <!-- </center> -->
                 </div>
               </div>
             </div>
